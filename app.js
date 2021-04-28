fa560ccf52d5a386d0214dc5396cfa37933190ad
Ryan Park <ryan.developer99@gmail.com>
2021-04-28 20:45:59 +0900

    add: click eventlistener
    add eventlistener on click button that shows 'clicked' on console

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 const input = document.getElementById("search-input");
 const btn = document.getElementById("btn");
 console.log(input.value);
+
+btn.addEventListener("click", () => {
+  console.log("clicked");
+});
