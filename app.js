e4a939c9df969b95b9407d242646176a1dc569dd
Ryan Park <ryan.developer99@gmail.com>
2021-04-28 20:48:48 +0900

    add: mediawiki api
    fetch mediawiki api

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ console.log(input.value);
 
 btn.addEventListener("click", () => {
   console.log("clicked");
+  const searchValue = input.value;
+  const url = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${searchValue}`;
+  console.log(url);
+  fetch(url)
+    .then((resp) => {
+      console.log(resp);
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 });
