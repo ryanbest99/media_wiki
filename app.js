1e4b4d67bbe78e9039b158b8c66dfec73789def1
Ryan Park <ryan.developer99@gmail.com>
2021-05-01 17:45:47 +0900

    fix: mediawiki api url

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-// Select elements
 const input = document.getElementById("search-input");
 const btn = document.getElementById("btn");
 console.log(input.value);
 
-// btn.addEventListener("click", () => {
-//   console.log("clicked");
-//   const searchValue = input.value;
-//   const url = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${searchValue}`;
-//   console.log(url);
-//   fetch(url)
-//     .then((resp) => {
-//       console.log(resp);
-//       return resp.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//     });
-// });
+const city = document.querySelector(".city");
 
 btn.addEventListener("click", () => {
   console.log("clicked");
   const searchValue = input.value;
-  const url = `https://en.wikipedia.org/w/api.php?action=query&query&prop=revisions&rvprop=content&format=json&titles=${searchValue}`;
+  const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&exintro&titles=${searchValue}&redirects=`;
   console.log(url);
   fetch(url)
     .then((resp) => {
@@ -32,11 +18,20 @@ btn.addEventListener("click", () => {
       console.log(data);
 
       let page = data.query.pages;
+      console.log(page);
       let pageId = Object.keys(data.query.pages);
       console.log(pageId);
-      let pageId2 = Object.keys(data.query.pages)[0];
-      console.log(pageId2);
-      let content = page[pageId].revisions[0]["*"];
-      console.log(content);
+
+      let aboutPage = page[pageId];
+      console.log(aboutPage);
+
+      let aboutCity = aboutPage.extract;
+      console.log(aboutCity);
+      console.log(typeof aboutCity);
+
+      let string = aboutCity.split(".");
+      console.log(string);
+
+      city.textContent = aboutPage.extract;
     });
 });
